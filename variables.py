2621aa365b1b00c815531547478a08532c054725
iovovo <iovovo@users.noreply.github.com>
2015-11-02 12:14:31 +0100

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+size=16
+border=0.5
+squareSize=[size,size]
+alive=1
+dead=0
+
+crawler = [-1,0,1]
+delay = 0.3
+window = 0                                             # glut window number
+width, height = 400, 400                               # window size
+multW, multH = width/size, height/size
